d875b0410a8c557babaac90c4e996e0d84edecf5
Anders Jenbo <anders@jenbo.dk>
2014-11-17 19:52:28 +0100

    Initial release

---
+++ CalcWorkDays.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Contains the class calcWorkDays
+ *
+ * PHP version 5.3
+ *
+ * @category Invoices
+ * @package  CalcWorkDays
+ * @author   Anders Jenbo <anders@jenbo.dk>
+ * @license  LGPL http://www.gnu.org/licenses/lgpl.html
+ * @link     http://anders.jenbo.dk/
+ */
+
+/**
+ * Class for calculating work dayes
+ *
+ * This class works with Unix timestamps and as such is limited to years
+ * beetwean 1970 and 2037
+ *
+ * @category Invoices
+ * @package  CalcWorkDays
+ * @author   Anders Jenbo <anders@jenbo.dk>
+ * @license  LGPL http://www.gnu.org/licenses/lgpl.html
+ * @link     http://anders.jenbo.dk/
+ */
+class CalcWorkDays
+{
+    // Country code from ISO 3166-2 to generate holidays for
+    static public $country = 'DK';
+
+    const ONEDAY = 86400;
+
+    // Cache calculated holidaies
+    static private $_holidays = [];
+
+    // Keep track of cache
+    static private $_cachedYears = [];
+
+    /**
+     * Add workdays to a time
+     *
+     * @param int $days Number of workdays to add/substract
+     * @param int $time The timestamp which is used as a base for the calculation
+     *
+     * @return int New timestamp
+     */
+    static public function addWorkDays($days, $time = null)
+    {
+        $time = is_null($time) ? time() : $time;
+        $days = (int) $days;
+        $_direction = $days == 0 ? 0 : $days / abs($days);
+        $_days = 0;
+
+        while ($_days !== $days) {
+            $time += $_direction * self::ONEDAY;
+            if (self::isWorkDay($time)) {
+                $_days += $_direction;
+            }
+        }
+
+        return $time;
+    }
+
+    /**
+     * Calculate the number of workdays between two dates
+     *
+     * @param int $start Starting timestamp for the calculation
+     * @param int $end   Ending timestamp for the calculation
+     *
+     * @return int Number of workdays
+     */
+    static public function workDaysBetween($start, $end)
+    {
+        $start = (int) $start;
+        $end = (int) $end;
+        $days = 0;
+
+        if ($start > $end) {
+            $tmp = $start;
+            $start = $end;
+            $end = $tmp;
+        }
+
+        while ($start <= $end) {
+            if (self::isWorkDay($start)) {
+                $days++;
+            }
+            $start += self::ONEDAY;
+        }
+
+        return $days;
+    }
+
+    /**
+     * Test if a given time is on a workday
+     *
+     * @param int $time The timestamp to test
+     *
+     * @return bool True if timestamp is in a workday
+     */
+    static public function isWorkDay($time = null)
+    {
+        $time = is_null($time) ? time() : $time;
+
+        $dow = date('w', $time);
+        if ($dow == 0 || $dow == 6) {
+            return false;
+        }
+
+        self::_cacheHolidays(date('Y', $time));
+        if (isset(self::$_holidays[self::$country][date('Y-m-d', $time)])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Generate cache of all holidays in a given year
+     *
+     * @param int $year Year to generate cache for
+     *
+     * @return null
+     */
+    static private function _cacheHolidays($year)
+    {
+        if (isset(self::$_cachedYears[self::$country][$year])) {
+            return;
+        }
+
+        switch (self::$country) {
+            case 'DK':
+                 // Holidays with a fixed date
+                self::$_holidays[self::$country][$year . '-01-01'] = true; // Nytårsdag
+                self::$_holidays[self::$country][$year . '-06-05'] = true; // Grundlovsdag
+                self::$_holidays[self::$country][$year . '-12-24'] = true; // Juleaften
+                self::$_holidays[self::$country][$year . '-12-25'] = true; // Juledag
+                self::$_holidays[self::$country][$year . '-12-26'] = true; // 2. Juledag
+
+                // Holidays that depends on the day of easter
+                $easter = easter_date($year);
+                $langfredag = $easter - self::ONEDAY;
+                while (date('w', $langfredag) != 5) {
+                    $langfredag -= self::ONEDAY;
+                }
+
+                self::$_holidays[self::$country][date('Y-m-d', $langfredag - self::ONEDAY)] = true; // Skærtorsdag
+                self::$_holidays[self::$country][date('Y-m-d', $langfredag)] = true; // Langfredag
+                self::$_holidays[self::$country][date('Y-m-d', $easter)] = true; // Påskedag
+                self::$_holidays[self::$country][date('Y-m-d', $easter + self::ONEDAY)] = true; // 2. Påskedag
+                self::$_holidays[self::$country][date('Y-m-d', $easter + self::ONEDAY * 26)] = true; // Store Bededag
+                self::$_holidays[self::$country][date('Y-m-d', $easter + self::ONEDAY * 39)] = true; // Kr. Himmelfart
+                self::$_holidays[self::$country][date('Y-m-d', $easter + self::ONEDAY * 49)] = true; // Pinsedag
+                self::$_holidays[self::$country][date('Y-m-d', $easter + self::ONEDAY * 50)] = true; // 2. Pinsedag
+                break;
+            default:
+                throw new Exception('Unsupported language');
+                break;
+        }
+
+        self::$_cachedYears[self::$country][$year] = true;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 CalcWorkDays.php
 ================
 
-Class for calculating workdayes
+Class for doing work day calculations.
+
+###Limitations:
+This class works with Unix timestamps and as such is limited to years beetwean 1970 and 2037
+
+###Samples
+How many days off work until xmas:
+
+`
+CalcWorkDays::workDaysBetween(
+  strtotime('today'),
+  strtotime('dec 24')
+)
+`
+
+Is tomorrow a work day:
+
+`
+CalcWorkDays::isWorkDay(strtotime('tomorrow'))
+`
+
+On what date will we have had 9 dayes of work from next monday:
+
+`
+CalcWorkDays::addWorkDays(9, strtotime('monday'))
+`
